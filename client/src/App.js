fd063230b45f1522825d7ff56a3125443dfbc2d5
Your Name <imdannycarter@gmail.com>
2022-12-03 00:13:35 -0800

    Test for routing went wrong, will have to use Redirects, not routes, problem will be solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Router} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Leaderboard from './pages/Leaderboard';
 import CodeIntake from './pages/CodeIntake';
 
+
+
+
 function App() {
-  const {isLoggedIn} = useSelector(state => state.auth)
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard/>}/>
           <Route path="/leaderboard" element={<Leaderboard/>}/>
-          {isLoggedIn ? <Router path="/codeintake" element={<CodeIntake/>}/>: <></>}
+          <Route path="/codeintake" element={<CodeIntake/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/CodeIntake.js
+++ client/src/pages/CodeIntake.js
@@ -1,8 +1,13 @@
 import React from 'react'
+//Redux
+import {useSelector} from 'react-redux';
 
 function CodeIntake() {
+  const {isLoggedIn} = useSelector(state => state.auth)
   return (
-    <div>CodeIntake</div>
+    <div>
+      {isLoggedIn ? <p>Enter code</p> : <p>You gotta login</p>}
+    </div>
   )
 }
 
