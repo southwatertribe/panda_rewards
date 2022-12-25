55c1665d627c23f87bfed05a43936a406d0ef056
southwatertribe <imdannycarter@gmail.com>
2022-12-24 23:24:35 -0800

    Begin leaderboard feature

---
+++ client/src/components/LeaderBoardItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function LeaderBoardItem(players) {
+  return (
+    <>
+            {
+                players.map((value, index) => (
+                    <div className="flex" key={index}>
+                        <div className="item">
+                            <img src={value.img} alt="" />
+            
+                            <div className="info">
+                                <h3 className='name text-dark'>{value.name}</h3>    
+                                <span>{value.location}</span>
+                            </div>                
+                        </div>
+                        <div className="item">
+                            <span>{value.score}</span>
+                        </div>
+                    </div>
+                    )
+                )
+            }
+        </>
+  )
+}
+
+export default LeaderBoardItem
