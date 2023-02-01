0d0ddc476bd931eedb3073ea8b362dc1c899351f
southwatertribe <imdannycarter@gmail.com>
2023-02-01 11:09:48 -0800

    Try this

--- server/queues/codeintake_queue.js
+++ server/queues/codeintake_queue.js
@@ -19,7 +19,35 @@ const codeIntakeQueue = new Queue(queueName, { redis: { port: redisPort, host: r
 //     console.log(`processing ${jobData}`)
 // })
 
-codeIntakeQueue.process(codeIntakeProcess.codeIntakeProcess)
+codeIntakeQueue.process(
+    async (job) => {
+    
+        console.log("You made it here")
+        // console.log("AND: " + job)
+        //Store the response, because we need to see if this worked in order to increment score in database
+        const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
+          
+            job
+        
+        })
+    
+        // //Debug DELETE IN PROD
+        // message = resp['data']
+        // console.log(message)
+        
+    
+        // if(message == "Sucess"){
+        //     //TODO INCREMENT SCORE
+        //     response = await dbFuncs.incrementScore(entry.user.user_email)
+    
+        // }
+    
+        // console.log(message)
+    
+        return message
+    
+    }
+)
 
 const sendCode = (data)=> {
     codeIntakeQueue.add(data, {
