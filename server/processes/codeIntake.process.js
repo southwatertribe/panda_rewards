dfda31f76b50103f533a60f65fdb4feb03ae8fd4
southwatertribe <imdannycarter@gmail.com>
2023-01-31 17:17:30 -0800

    Seems like bull has an issue

--- server/processes/codeIntake.process.js
+++ server/processes/codeIntake.process.js
@@ -1,31 +1,31 @@
-// import { Job } from 'bull';
+// // import { Job } from 'bull';
 
-const Job = require('bull')
+// const Job = require('bull')
 
-const codeIntakeProcess = async (job) => {
+// const codeIntakeProcess = async (job) => {
 
-    //Store the response, because we need to see if this worked in order to increment score in database
-    const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
+//     //Store the response, because we need to see if this worked in order to increment score in database
+//     const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
       
-        job
+//         job
     
-    })
+//     })
 
-    //Debug DELETE IN PROD
-    message = resp['data']
-    console.log(message)
+//     //Debug DELETE IN PROD
+//     message = resp['data']
+//     console.log(message)
     
 
-    if(message == "Sucess"){
-        //TODO INCREMENT SCORE
-        response = await dbFuncs.incrementScore(entry.user.user_email)
+//     if(message == "Sucess"){
+//         //TODO INCREMENT SCORE
+//         response = await dbFuncs.incrementScore(entry.user.user_email)
 
-    }
+//     }
 
-    return message
+//     return message
 
-}
+// }
 
 
-module.exports = {codeIntakeProcess}
-// export default codeIntakeProcess;
+// module.exports = {codeIntakeProcess}
+// // export default codeIntakeProcess;
