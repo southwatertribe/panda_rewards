706c3805e7f02760f5c96fdba94e044da9cdbfe8
southwatertribe <imdannycarter@gmail.com>
2023-01-31 17:58:59 -0800

    retry redis

--- server/queues/codeintake_queue.js
+++ server/queues/codeintake_queue.js
@@ -1,18 +1,18 @@
-// // import Bull from 'bull'
-// const Bull = require('bull')
-// // import codeIntakeProcess from '../processes/codeIntake.process'
+// import Bull from 'bull'
+const Bull = require('bull')
+// import codeIntakeProcess from '../processes/codeIntake.process'
 
-// const codeIntakeProcess = require('../processes/codeIntake.process')
-// const codeIntakeQueue = new Bull('codeIntake', {
-//     redis: "redis://127.0.0.1:6379"
-// })
+const codeIntakeProcess = require('../processes/codeIntake.process')
+const codeIntakeQueue = new Bull('codeIntake', {
+    redis: "redis://127.0.0.1:6379"
+})
 
-// codeIntakeQueue.process(codeIntakeProcess)
+codeIntakeQueue.process(codeIntakeProcess)
 
-// const sendCode = (data)=> {
-//     codeIntakeQueue.add(data, {
+const sendCode = (data)=> {
+    codeIntakeQueue.add(data, {
         
-//     })
-// }
+    })
+}
 
-// module.exports = { sendCode}
+module.exports = { sendCode}
