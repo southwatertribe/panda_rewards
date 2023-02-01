5c8ab7d22a0753a2537ec821485b3a79c85db53f
southwatertribe <imdannycarter@gmail.com>
2023-02-01 10:57:54 -0800

    Redis on docker

--- server/queues/codeintake_queue.js
+++ server/queues/codeintake_queue.js
@@ -1,11 +1,23 @@
 // import Bull from 'bull'
 const Queue = require('bull')
 // import codeIntakeProcess from '../processes/codeIntake.process'
-
 const codeIntakeProcess = require('../processes/codeIntake.process')
-const codeIntakeQueue = new Queue('codeIntake')
 
-//
+
+const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+const redisPort = process.env.REDIS_PORT || 6379;
+const intervalInMilli = 1000; // 1000 milliseconds;
+const queueName = 'code_intake';
+
+const codeIntakeQueue = new Queue(queueName, { redis: { port: redisPort, host: redisHost } });
+
+// const codeIntakeQueue = new Queue('codeIntake')
+
+
+// codeIntakeQueue.process(function (job,done) {
+//     const jobData = job.data;
+//     console.log(`processing ${jobData}`)
+// })
 
 codeIntakeQueue.process(codeIntakeProcess.codeIntakeProcess)
 
