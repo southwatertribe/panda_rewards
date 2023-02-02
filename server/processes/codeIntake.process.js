ec18dc4b27288db819088b6c1a6e8898c0b0d4e4
southwatertribe <imdannycarter@gmail.com>
2023-02-01 19:06:41 -0800

    Lets test again

--- server/processes/codeIntake.process.js
+++ server/processes/codeIntake.process.js
@@ -1,15 +1,13 @@
 // import { Job } from 'bull';
 
-const Job = require('bull')
-
-const codeIntakeProcess = async (Job) => {
+const codeIntakeProcess = async (job) => {
     
     console.log("You made it here")
     // console.log("AND: " + job)
     //Store the response, because we need to see if this worked in order to increment score in database
     const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
       
-        Job
+        job
     
     })
 
