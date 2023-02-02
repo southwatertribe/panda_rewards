e600b312bf07531ecb0e4a69df9960dab55e12d0
southwatertribe <imdannycarter@gmail.com>
2023-02-01 19:31:39 -0800

    More debug

--- server/queues/codeintake_queue.js
+++ server/queues/codeintake_queue.js
@@ -22,38 +22,39 @@ const codeIntakeQueue = new Queue(queueName, { redis: { port: redisPort, host: r
 // })
 
 codeIntakeQueue.process( 
-    async (job)=> {
+    function (data){
     
         console.log("You made it here")
         // console.log("AND: " + job)
         //Store the response, because we need to see if this worked in order to increment score in database
-        const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
+        // const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
           
-            job
+        //     data
         
-        })
+        // })
     
         //Debug DELETE IN PROD
-        message = resp['data']
-        console.log(message)
+        // message = resp['data']
+        // console.log(message)
         
     
-        if(message == "Sucess"){
-            //TODO INCREMENT SCORE
-            response = await dbFuncs.incrementScore(entry.user.user_email)
+        // if(message == "Sucess"){
+        //     //TODO INCREMENT SCORE
+        //     response = await dbFuncs.incrementScore(entry.user.user_email)
     
-        }
+        // }
     
-        console.log(message)
+        // console.log(message)
     
-        return message
+        // return message
+        return 'ok'
     
     }
 )
 
-const sendCode =  (data)=> {
+const sendCode =  async (data)=> {
     console.log("Entering Queue")
-    codeIntakeQueue.add(data)
+    await codeIntakeQueue.add(data)
 }
 
 
