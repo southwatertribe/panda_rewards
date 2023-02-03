1b1eed430dc2456328d0882b02bbcb2f17b1d6e3
southwatertribe <imdannycarter@gmail.com>
2023-02-03 10:45:03 -0800

    Continuing testing

--- server/queues/codeintake_queue.js
+++ server/queues/codeintake_queue.js
@@ -6,7 +6,7 @@ const codeIntakeProcess = require('../processes/codeIntake.process')
 
 
 
-const redisHost = process.env.REDIS_HOST || '0.0.0.0';
+const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || 6379;
 const intervalInMilli = 1000; // 1000 milliseconds;
 const queueName = 'code_intake';
