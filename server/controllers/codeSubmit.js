d2a3315e213c167635626c8db028dae898e59b2e
southwatertribe <imdannycarter@gmail.com>
2023-02-26 00:34:20 -0800

    Test promise then

--- server/controllers/codeSubmit.js
+++ server/controllers/codeSubmit.js
@@ -4,27 +4,43 @@ const dbFuncs = require("../db/dynamo.js")
 const codeSubmit = async (entry) => {
     
 
-    //Store the response, because we need to see if this worked in order to increment score in database
-    const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
-      
+    // //Store the response, because we need to see if this worked in order to increment score in database
+    // const resp = await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
+    
+    //     entry
+    
+    // })
+
+    await axios.post("https://6gzwnr5576chzorjos2vmii4jq0kzunw.lambda-url.us-west-1.on.aws/", {
+    
         entry
     
-    })
+    }).then((result) => {
+
+        message = result['data']
+
+        console.log(`In the then: ${message}`)
+        
+        return message
+    }).catch((err) => {
+        
+    });
 
     //Debug DELETE IN PROD
-    message = resp['data']
-    console.log(message)
+    // message = resp['data']
+    // console.log(message)
     
 
-    if(message == "Sucess"){
-        //TODO INCREMENT SCORE
-        response = await dbFuncs.incrementScore(entry.user.user_email)
+    // if(message == "Sucess"){
+    //     //TODO INCREMENT SCORE
+    //     response = await dbFuncs.incrementScore(entry.user.user_email)
 
-    }
+    // }
 
-    return message
+    // return message
 
 }
+
 
 
 module.exports = {codeSubmit}
